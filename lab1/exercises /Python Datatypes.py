5564e8059f5824231e8f3c37e49266a567090047
AnsatK <156984817+AnsatK@users.noreply.github.com>
2024-01-25 12:27:49 +0600

    Update Python Datatypes.py

--- lab1/exercises /Python Datatypes.py
+++ lab1/exercises /Python Datatypes.py
@@ -2,5 +2,40 @@
 x = 5
 print(type(x))
 
-int
+# ouput: int
+
 #2
+x = 5
+print(type(x))
+
+#output: str
+
+#3
+x = 20.5
+print(type(x))
+
+#output: float 
+
+#4
+x = ["apple", "banana", "cherrry"]
+print(type(x))
+
+#output: list
+
+#5
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#ouput: tuple
+
+#6 
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#ouput: dict
+
+#7
+x = True
+print(type(x))
+
+#output: bool
